1912c64583c88a04567a40434ef752705317bf2e
Kelven Galvão <kelvengalvao@gmail.com>
2020-07-11 14:27:06 -0300

    Add colors

---
+++ lib/src/shared/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color orange = Color(0xffE07C58);
+  static const Color backgroundBlue = Color(0xff1B2037);
+  static const Color bottomBarBlue = Color(0xff23283E);
+  static const Color beige = Color(0xffF4E6CD);
+  static const Color lightBrown = Color(0xffB08475);
+  static const Color brown = Color(0xff4F2E2D);
+  static const Color grey = Color(0xff7D7F88);
+  static const Color lightGrey = Color(0xffCDCDCD);
+}
